757d45e784b0e2b6d1f58b059d2c370b323632fa
robylosa94 <roby94.losa@tiscali.it>
2024-12-27 18:11:09 +0100

    add vuetify and manage theme toggle

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-12-27',
+  devtools: { enabled: true },
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
+
+  build: {
+    transpile: ['vuetify'], // Assicura che Vuetify venga transpile
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false, // Necessario per Vuetify
+    },
+  },
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Welcome to Budget Manager App</h1>
+  <v-row>
+    <v-col cols="12" lg="2">
+      <v-btn color="primary">Entrate</v-btn>
+    </v-col>
+    <v-col cols="12" lg="2">
+      <v-btn color="secondary">Uscite</v-btn>
+    </v-col>
+  </v-row>
 </template>
